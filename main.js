8913614a9fb041875b315dd7a2c64c3c802904ac
sukumar571 <suku0571@gmail.com>
2024-05-31 10:49:35 +0530

    portfolio

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new typed(".text", {
+    Strings: ["frontend developer" , "youtuber" , "web developer"],
+    typespeed: 100,
+    backspeed: 100,
+    backdelay: 1000,
+    loop: true,
+});
